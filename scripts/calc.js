2a846c8faf6c63ce54cfd45f062b1463a93a06cf
AZigalovs <alexzh25@gmail.com>
2019-11-16 17:52:43 +0000

    setup spec and fix test

--- scripts/calc.js
+++ scripts/calc.js
@@ -1,13 +1,12 @@
-Calucator = function () {
+Calculator = function () {
     this.value = 0;
 }
 
-Calucator.prototype.add = function(number) {
+Calculator.prototype.add = function(number) {
     if (typeof(number) == "number") {
         this.value += number; 
     } else {
         alert("Error!");
     }
-        
+} 
     
-}
